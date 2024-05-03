a6fc5f198812e3ba144cbf8b0d35f4dfc185718c
alejandro979 <63746669+alejandro979@users.noreply.github.com>
2024-05-03 00:06:34 +0000

    modified:   Hello.py

--- Hello.py
+++ Hello.py
@@ -16,7 +16,7 @@ from yaml.loader import SafeLoader
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
